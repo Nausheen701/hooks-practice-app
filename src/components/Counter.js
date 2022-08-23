15665294d1168e22a9412e8c76bbcd190613b3c8
Nausheen Akhter <nausheen701@gmail.com>
2022-08-22 20:33:20 -0400

    useState and setClicks

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Counter = () => {
+    const [clicks, setClicks] = useState(0)
+
+    function handleButtonClick() {
+        setClicks(clicks+1)
+    }
+    
   return (
-    <span>You clicked ** clicks</span>>
+    <> 
+    <span>You clicked {clicks} clicks</span>
     <br/>
     <button onClick={handleButtonClick}>Click me!</button>
+    </>
   )
 }
 
 export default Counter
+
+
+ // handleButtonClick = () => {     
+    //     setClicks(clicks+1)
+    // }
